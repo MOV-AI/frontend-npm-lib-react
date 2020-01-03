5295004868c729c02fc13a16fe74e5ae8089e6e5
Vicente Queiroz <amcbd89@bitbucket.org>
2020-01-03 11:28:32 +0000

    Completely removed Authentication/database/etc...

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-import Authentication from "./src/Authentication/Authentication";
-import Utils from "./src/Utils/Utils";
-import Database from "./src/Database/Database";
-import MasterDB from "./src/Database/MasterDB";
-import AuthWebSocket from "./src/AuthWebSocket/AuthWebSocket";
 import Button from "./src/Components/Button";
 
-export { Authentication, Utils, Database, MasterDB, AuthWebSocket, Button };
+export { Button };
