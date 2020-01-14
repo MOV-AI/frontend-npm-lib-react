b84ab4aecf0df57483542beb31eceb5c8ae28070
Vicente Queiroz <amcbd89@bitbucket.org>
2020-01-14 17:36:08 +0000

    Run build

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import AbstractModal from "./src/Components/AbstractModal";
 import Drawer from "./src/Components/Drawer";
 import Collapse from "./src/Components/Collapse";
 import VerticalBar from "./src/Components/VerticalBar";
+import ContextMenu from "./src/Components/ContextMenu";
 
-export { Button, AbstractModal, Drawer, Collapse, VerticalBar };
+export { Button, AbstractModal, Drawer, Collapse, VerticalBar, ContextMenu };
