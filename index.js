ede47361be50b37eab2a70822517060426565ad7
cristovao <pedro.cristovao@mov.ai>
2020-01-15 15:05:22 +0000

    export viewer

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ import Button from "./src/Components/Button";
 import AbstractModal from "./src/Components/AbstractModal";
 import Drawer from "./src/Components/Drawer";
 import Collapse from "./src/Components/Collapse";
+import SceneViewer from "./src/Components/Viewer/SceneViewer";
 
-export { Button, AbstractModal, Drawer, Collapse };
+export { Button, AbstractModal, Drawer, Collapse, SceneViewer };
