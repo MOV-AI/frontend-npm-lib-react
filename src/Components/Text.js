a7bef9935c65be82234ce128249f2524c78249d3
Vicente Queiroz <amcbd89@bitbucket.org>
2020-01-22 17:35:35 +0000

    Text: refactor

--- src/Components/Text.js
+++ src/Components/Text.js
@@ -3,14 +3,10 @@ import TextField from "@material-ui/core/TextField";
 import PropTypes from "prop-types";
 
 const Text = props => {
-  return <TextField {...props}>{props.children}</TextField>;
+  return <TextField {...props}></TextField>;
 };
 
-Text.propTypes = {
-  children: PropTypes.node.isRequired
-};
-Text.defaultProps = {
-  children: <div></div>
-};
+Text.propTypes = {};
+Text.defaultProps = {};
 
 export default Text;
