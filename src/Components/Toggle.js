fd257dfe39648748900fce72802c96767883f402
Vicente Queiroz <amcbd89@bitbucket.org>
2020-01-30 15:41:59 +0000

    Added Toggle Input

---
+++ src/Components/Toggle.js
@@ -0,0 +1,64 @@
+import React from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import { makeStyles } from "@material-ui/core/styles";
+import { green, grey } from "@material-ui/core/colors";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles(theme => ({
+  colorSwitchBase: {
+    color: grey[300],
+    "&$colorChecked": {
+      color: theme.palette.primary.main, //green[500]
+      "& + $colorBar": {
+        backgroundColor: theme.palette.primary.main //green[500]
+      }
+    }
+  },
+  label: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "14px"
+  },
+  colorBar: {},
+  colorChecked: { "&hover": { backgroundColor: theme.palette.primary.main } }
+}));
+
+const Toggle = props => {
+  const classes = useStyles();
+  console.log("props", props);
+  return (
+    <FormControlLabel
+      control={
+        <Switch
+          checked={props.toggle}
+          onChange={props.onToggle}
+          color={props.color}
+          //   classes={{
+          //     switchBase: classes.colorSwitchBase,
+          //     checked: classes.colorChecked,
+          //     track: classes.colorBar
+          //   }}
+        />
+      }
+      labelPlacement="start"
+      label={props.label}
+      classes={{ label: classes.label }}
+      style={{ margin: 0, display: "flex", justifyContent: "center" }}
+    />
+  );
+};
+
+Toggle.propTypes = {
+  toggle: PropTypes.bool,
+  onToggle: PropTypes.func,
+  label: PropTypes.string,
+  color: PropTypes.string
+};
+Toggle.defaultProps = {
+  label: undefined,
+  color: "primary" // or secondary
+};
+
+export default Toggle;
