e14223d5ae7f7b322b4da4855b6496f6a231e841
Vicente Queiroz <amcbd89@bitbucket.org>
2020-01-31 11:21:58 +0000

    Added Select

---
+++ stories/Select.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Select from "../src/Components/Select";
+
+export default {
+  title: "Select"
+};
+
+export const select = () => <Select></Select>;
+
+select.story = {
+  name: "simple select"
+};
