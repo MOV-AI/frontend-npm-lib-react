fbec8eb1d6afe41b40c20f08c2fe4619d18b749c
Vicente Queiroz <amcbd89@bitbucket.org>
2020-02-27 17:24:06 +0000

    Added Jose's theme

--- src/styles/Themes.js
+++ src/styles/Themes.js
@@ -2,74 +2,28 @@ import { createMuiTheme } from "@material-ui/core/styles";
 
 const themeFactory = particular => ({
   ...particular,
-  //spacing: value => `${value ** 2}px`,
-  fontSize: value => `${[14, 16, 17][value]}px`,
   typography: {
+    fontFamily: "Roboto",
     h1: {
-      fontFamily: "Avenir",
-      fontSize: "96px",
-      fontWeight: 300
+      fontFamily: "Avenir"
     },
     h2: {
-      fontFamily: "Avenir",
-      fontSize: "60px",
-      fontWeight: 300
+      fontFamily: "Avenir"
     },
     h3: {
-      fontFamily: "Avenir",
-      fontSize: "48px",
-      fontWeight: 400
+      fontFamily: "Avenir"
     },
     h4: {
-      fontFamily: "Avenir",
-      fontSize: "34px",
-      fontWeight: 400
+      fontFamily: "Avenir"
     },
     h5: {
-      fontFamily: "Avenir",
-      fontSize: "24px",
-      fontWeight: 400
+      fontFamily: "Avenir"
     },
     h6: {
-      fontFamily: "Avenir",
-      fontSize: "20px",
-      fontWeight: 600
-    },
-    subtitle1: {
-      fontFamily: "Roboto",
-      fontSize: "16px",
-      fontWeight: 400
-    },
-    subtitle2: {
-      fontFamily: "Roboto",
-      fontSize: "14px",
-      fontWeight: 600
+      fontFamily: "Avenir"
     },
     body1: {
-      fontFamily: "Roboto",
-      fontSize: "16px",
-      fontWeight: 400
-    },
-    body2: {
-      fontFamily: "Roboto",
-      fontSize: "14px",
-      fontWeight: 400
-    },
-    button: {
-      fontFamily: "Roboto",
-      fontSize: "14px",
-      fontWeight: 600,
-      textTransform: "uppercase"
-    },
-    caption: {
-      fontFamily: "Roboto",
-      fontSize: "12px",
-      fontWeight: 400
-    },
-    overline: {
-      fontFamily: "Roboto",
-      fontSize: "10px",
-      fontWeight: 400
+      fontFamily: "Roboto"
     }
   }
 });
@@ -114,23 +68,80 @@ const Themes = {
       palette: {
         type: "dark", // Switching the dark mode on, is a single property value change.
         primary: {
-          main: "#2090B7",
-          light: "rgb(76, 166, 197)",
-          dark: "rgb(22, 100, 128)"
+          main: "#36b5e6"
         },
-        action: {
-          hoverOpacity: 0.08,
-          hover: "rgba(32, 144, 183, 0.08)",
-          selected: "rgba(32, 144, 183, 0.14)",
-          disabledBackground: "rgba(32, 144, 183, 0.12)",
-          disabled: "rgba(32, 144, 183, 0.26)",
-          active: "rgba(32, 144, 183, 0.54)"
+        secondary: {
+          main: "#CF6679"
         },
-        text: {
-          primary: "rgba(255, 255, 255, 0.87)",
-          secondary: "rgba(255, 255, 255, 0.54)",
-          disabled: "rgba(255, 255, 255, 0.38)",
-          hint: "rgba(255, 255, 255, 0.38)"
+        green: {
+          main: "#03DAC5"
+        }
+      },
+      overrides: {
+        MuiButton: {
+          outlined: {
+            margin: "8px"
+          },
+          text: {
+            margin: "8px"
+          },
+          contained: {
+            margin: "8px"
+          },
+          containedPrimary: {
+            color: "inherit",
+            margin: "8px"
+          },
+          containedSecondary: {
+            color: "inherit",
+            margin: "8px"
+          }
+        },
+        MuiButtonGroup: {
+          root: {
+            margin: "0"
+          },
+          groupedContainedPrimary: {
+            margin: "0"
+          },
+          groupedOutlined: {
+            margin: "0"
+          },
+          groupedText: {
+            margin: "0"
+          }
+        },
+        MuiIconButton: {
+          root: {
+            color: "#36b5e6"
+          }
+        },
+        MuiListItem: {
+          button: {
+            "&:hover": {
+              backgroundColor: "rgba(54,181,230, 0.15)"
+            }
+          }
+        },
+        MuiFormControlLabel: {
+          label: {
+            color: "rgba(255,255,255,0.8)"
+          }
+        },
+        MuiInputBase: {
+          input: {
+            font: "Roboto"
+          }
+        },
+        MuiFilledInput: {
+          root: {
+            border: "4px"
+          },
+          underline: {
+            "&::before": {
+              borderRadius: "4px"
+            }
+          }
         }
       }
     })
@@ -172,25 +183,81 @@ const Themes = {
         upperTextColor: "#474747"
       },
       palette: {
-        type: "light",
         primary: {
-          main: "#007097",
-          light: "rgb(51, 141, 171)",
-          dark: "rgb(0, 79, 105)"
+          main: "#007197"
         },
-        action: {
-          hoverOpacity: 0.08,
-          hover: "rgba(0, 112, 151, 0.08)",
-          selected: "rgba(0, 112, 151, 0.14)",
-          disabledBackground: "rgba(0, 112, 151, 0.12)",
-          disabled: "rgba(0, 112, 151, 0.26)",
-          active: "rgba(0, 112, 151, 0.54)"
+        secondary: {
+          main: "#BE2424"
         },
-        text: {
-          primary: "rgba(0, 0, 0, 0.87)",
-          secondary: "rgba(0, 0, 0, 0.54)",
-          disabled: "rgba(0, 0, 0, 0.38)",
-          hint: "rgba(0, 0, 0, 0.38)"
+        green: {
+          main: "#03DAC5"
+        }
+      },
+      overrides: {
+        MuiButton: {
+          outlined: {
+            margin: "8px"
+          },
+          text: {
+            margin: "8px"
+          },
+          contained: {
+            margin: "8px"
+          },
+          containedPrimary: {
+            color: "white",
+            margin: "8px"
+          },
+          containedSecondary: {
+            color: "white",
+            margin: "8px"
+          }
+        },
+        MuiButtonGroup: {
+          root: {
+            margin: "0"
+          },
+          groupedContainedPrimary: {
+            margin: "0"
+          },
+          groupedOutlined: {
+            margin: "0"
+          },
+          groupedText: {
+            margin: "0"
+          }
+        },
+        MuiIconButton: {
+          root: {
+            color: "#007197"
+          }
+        },
+        MuiListItem: {
+          button: {
+            "&:hover": {
+              backgroundColor: "rgba(0,113,151, 0.15)"
+            }
+          }
+        },
+        MuiFormControlLabel: {
+          label: {
+            color: "rgba(0,0,0,0.8)"
+          }
+        },
+        MuiInputBase: {
+          input: {
+            font: "Roboto"
+          }
+        },
+        MuiFilledInput: {
+          root: {
+            border: "4px"
+          },
+          underline: {
+            "&::before": {
+              borderRadius: "4px"
+            }
+          }
         }
       }
     })
