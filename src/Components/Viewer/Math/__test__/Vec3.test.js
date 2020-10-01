35e905a8b3e713215c72a6e0e13362abfa8aa861
cristovao <pedro.cristovao@mov.ai>
2020-10-01 12:58:20 +0100

    update math

--- src/Components/Viewer/Math/__test__/Vec3.test.js
+++ src/Components/Viewer/Math/__test__/Vec3.test.js
@@ -3,35 +3,24 @@ import { Vector3 } from "@babylonjs/core";
 
 test("test vec add", () => {
   expect(
-    new Vec3([1, 0, 0])
-      .add(new Vec3([0, 1, 0]))
-      .add(new Vec3([0, 0, 1]))
-      .equals(new Vec3([1, 1, 1]))
-  ).toBeTruthy();
+    new Vec3([1, 0, 0]).add(new Vec3([0, 1, 0])).add(new Vec3([0, 0, 1]))
+  ).toCustomEqual(new Vec3([1, 1, 1]));
 });
 
 test("test vec sub", () => {
   expect(
-    new Vec3([1, 0, 0])
-      .sub(new Vec3([0, 1, 0]))
-      .sub(new Vec3([0, 0, 1]))
-      .equals(new Vec3([1, -1, -1]))
-  ).toBeTruthy();
+    new Vec3([1, 0, 0]).sub(new Vec3([0, 1, 0])).sub(new Vec3([0, 0, 1]))
+  ).toCustomEqual(new Vec3([1, -1, -1]));
 });
 
 test("test vec mul", () => {
   expect(
-    new Vec3([1, 1, 1])
-      .mul(new Vec3([1, -1, 0]))
-      .add(new Vec3([2, 1, 1]))
-      .equals(new Vec3([2, -1, 0]))
-  ).toBeTruthy();
+    new Vec3([1, 1, 1]).mul(new Vec3([1, -1, 0])).add(new Vec3([2, 1, 1]))
+  ).toCustomEqual(new Vec3([2, -1, 0]));
 });
 
 test("test vec scale", () => {
-  expect(
-    new Vec3([1, 1, 1]).scale(10).equals(new Vec3([10, 10, 10]))
-  ).toBeTruthy();
+  expect(new Vec3([1, 1, 1]).scale(10)).toCustomEqual(new Vec3([10, 10, 10]));
 });
 
 test("test vec length", () => {
@@ -39,9 +28,9 @@ test("test vec length", () => {
 });
 
 test("test vec creation of Babylon", () => {
-  expect(
-    Vec3.ofBabylon(new Vector3(1, 1, 1)).equals(Vec3.of([1, 1, 1]))
-  ).toBeTruthy();
+  expect(Vec3.ofBabylon(new Vector3(1, 1, 1))).toCustomEqual(
+    Vec3.of([1, 1, 1])
+  );
 });
 
 test("test vec to array", () => {
