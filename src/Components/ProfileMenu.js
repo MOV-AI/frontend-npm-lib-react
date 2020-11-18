c6f22a7b625f63e31aa3c42b0666a017bd964afe
matheus <matheus.fernandesww@mov.ai>
2020-11-18 15:42:18 +0000

    Added array functionality to ProfileMenu and run build

--- src/Components/ProfileMenu.js
+++ src/Components/ProfileMenu.js
@@ -2,6 +2,9 @@ import React from "react";
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import PropTypes from "prop-types";
+import SettingsIcon from '@material-ui/icons/Settings';
+import Switch from '@material-ui/core/Switch';
 
 const ProfileMenu = props => {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -14,10 +17,12 @@ const ProfileMenu = props => {
         setAnchorEl(null);
     };
 
+    // const {profile} = props;
+
     return (
         <div>
             <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                Open Menu
+                <SettingsIcon />
             </Button>
             <Menu
                 id="simple-menu"
@@ -26,11 +31,45 @@ const ProfileMenu = props => {
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
             >
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                <div>Hello, {props.name || "User"}</div>
+                {props.extraItems?.map((item) => (
+                    <MenuItem onClick={() => item.func}>{item.label}</MenuItem>
+                ))}
+                <MenuItem>Dark Theme 
+                    <Switch
+                        checked={props.themeStatus}
+                        onChange={props.handleToggleTheme}
+                        color="primary"
+                        name="Dark Theme"
+                        inputProps={{ 'aria-label': 'checkbox' }}
+                    />
+                </MenuItem>
+                <MenuItem onClick={props.handleLogout}>Logout</MenuItem>
+                <div>{window.SERVER_DATA?.Application.Version || 'v.1.0.2020'}</div>
             </Menu>
         </div>
     );
 };
+    ProfileMenu.propTypes = {
+        style: PropTypes.object,
+        onClick: PropTypes.func,
+        color: PropTypes.string,
+        variant: PropTypes.string.isRequired,
+        size: PropTypes.string,
+        startIcon: PropTypes.element,
+        children: PropTypes.node.isRequired,
+        disabled: PropTypes.bool
+      };
+    
+      ProfileMenu.defaultProps = {
+        style: {},
+        onClick: () => console.log("Click MOV.AI ProfileMenu"),
+        color: "default", // default, inherit, primary or secondary
+        variant: "contained", // text, outlined, contained
+        size: "medium", // small, medium, large
+        startIcon: undefined,
+        children: <div></div>,
+        disabled: false
+      };
 
 export default ProfileMenu;
