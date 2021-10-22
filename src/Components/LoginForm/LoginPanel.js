10dd15943e3b4c36e6c31f5af580b98e1c37f25b
Manuel Silva <manuel.silva@mov.ai>
2021-10-22 23:51:32 +0100

    FP-839-Apps-accessible-without-permissions
    - withAuthentication
    	- Refactor code
    	- Add ability to update refresh token and access token if user already has the refresh token
    	- Validate access to the application
    
    - Add a messages panel
    	- show progress while the user is authenticated
    	- show that the user is not allowed to access the application

---
+++ src/Components/LoginForm/LoginPanel.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import defaultLogo from "./movai_red.svg";
+import Paper from "@material-ui/core/Paper";
+import { withStyles } from "@material-ui/core/styles";
+import { LinearProgress } from "@material-ui/core";
+import PropTypes from "prop-types";
+
+const styles = theme => ({
+  root: {
+    padding: theme.spacing(4, 4),
+    borderRadius: 40
+  }
+});
+
+class NotAuthorized extends Component {
+  render() {
+    const { classes, logo, title, message, progress } = this.props;
+
+    return (
+      <Grid
+        style={{ paddingTop: "5%" }}
+        container
+        direction="column"
+        alignItems="center"
+        justify="space-evenly"
+        justifyContent="flex-end"
+      >
+        <Paper elevation={10} className={classes.root}>
+          <Grid item>
+            <img
+              src={logo}
+              className="center"
+              alt="logo"
+              style={{
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+                width: "50%"
+              }}
+            />
+          </Grid>
+          <Grid>
+            <Typography align="center" variant="h2" gutterBottom>
+              {title}
+            </Typography>
+            <Typography align="center" variant="subtitle1" gutterBottom>
+              {message}
+              {progress && <LinearProgress />}
+            </Typography>
+          </Grid>
+        </Paper>
+      </Grid>
+    );
+  }
+}
+
+NotAuthorized.propTypes = {
+  logo: PropTypes.any, // expects a svg element
+  title: PropTypes.string,
+  message: PropTypes.string,
+  progress: PropTypes.bool
+};
+
+NotAuthorized.defaultProps = {
+  logo: defaultLogo,
+  title: "",
+  message: "",
+  progress: false
+};
+
+export default withStyles(styles, { withTheme: true })(NotAuthorized);
