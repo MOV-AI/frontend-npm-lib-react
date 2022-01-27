95ea3e80355f9b154998d2b20712834a38c7cd03
Jose Sampaio <jose.sampaio@mov.ai>
2022-01-27 18:41:36 +0000

    Support multiple authentications on login form

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -2,13 +2,19 @@ import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Collapse from "@material-ui/core/Collapse";
 import defaultLogo from "./movai_red.svg";
 import Paper from "@material-ui/core/Paper";
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
+import List from "@material-ui/core/List";
 import { withStyles } from "@material-ui/core/styles";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import { Authentication } from "@mov-ai/mov-fe-lib-core";
 import PropTypes from "prop-types";
 
@@ -16,6 +22,9 @@ const styles = theme => ({
   root: {
     padding: theme.spacing(4, 4),
     borderRadius: 40
+  },
+  formControl: {
+    width: "50%"
   }
 });
 
@@ -26,7 +35,9 @@ class LoginForm extends Component {
     remember: false,
     error: false,
     errorMessage: "",
-    capsLockOn: false
+    capsLockOn: false,
+    advancedMenuOpen: false,
+    authenticationProvider: Authentication.DEFAULT_PROVIDER
   };
 
   sendCreds = async () => {
@@ -35,7 +46,8 @@ class LoginForm extends Component {
       const apiResponse = await Authentication.login(
         this.state.username,
         this.state.password,
-        this.state.remember
+        this.state.remember,
+        this.state.authenticationProvider
       );
       // If successfully logged in
       if (!apiResponse.error) {
@@ -64,9 +76,20 @@ class LoginForm extends Component {
     }
   };
 
-  render() {
-    const { classes, logo } = this.props;
+  handleAuthenticationProviderChange = e => {
+    this.setState({
+      authenticationProvider: e.target.value
+    });
+  };
 
+  handleAdvancedMenuToggle = () => {
+    this.setState({
+      advancedMenuOpen: !this.state.advancedMenuOpen
+    });
+  };
+
+  render() {
+    const { classes, logo, authenticationProviders } = this.props;
     return (
       <Grid
         style={{ paddingTop: "5%" }}
@@ -156,6 +179,63 @@ class LoginForm extends Component {
             </Typography>
           </Grid>
           <Grid>
+            <Typography align="center" variant="subtitle1" gutterBottom>
+              <div style={{ flexGrow: 1 }}>
+                <Button
+                  className={classes.formControl}
+                  endIcon={
+                    this.state.advancedMenuOpen ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    )
+                  }
+                  onClick={this.handleAdvancedMenuToggle}
+                  disabled={!authenticationProviders?.length}
+                >
+                  Advanced
+                </Button>
+                {authenticationProviders?.length > 0 && (
+                  <Collapse in={this.state.advancedMenuOpen}>
+                    <List dense={true} component="div">
+                      <Typography component="div">
+                        <Grid
+                          container
+                          style={{ padding: "20px", justifyContent: "center" }}
+                        >
+                          <FormControl
+                            className={classes.formControl}
+                            error={
+                              this.state.authenticationProvider == null ||
+                              this.state.authenticationProvider == undefined
+                            }
+                          >
+                            <InputLabel htmlFor="component-authentication-selector">
+                              Authentication
+                            </InputLabel>
+                            <Select
+                              id="component-authentication-selector"
+                              aria-describedby="component-authentication-error-text"
+                              value={this.state.authenticationProvider}
+                              onChange={this.handleAuthenticationProviderChange}
+                              disabled={false}
+                            >
+                              {authenticationProviders.map(({ id, text }) => (
+                                <MenuItem key={id} value={id}>
+                                  {text}
+                                </MenuItem>
+                              ))}
+                            </Select>
+                          </FormControl>
+                        </Grid>
+                      </Typography>
+                    </List>
+                  </Collapse>
+                )}
+              </div>
+            </Typography>
+          </Grid>
+          <Grid>
             <Typography align="center" gutterBottom>
               <Button onClick={() => this.sendCreds()}>Login</Button>
             </Typography>
@@ -167,11 +247,13 @@ class LoginForm extends Component {
 }
 
 LoginForm.propTypes = {
+  authenticationProviders: PropTypes.array,
   logo: PropTypes.any, // expects a svg element
   setLoggedIn: PropTypes.func
 };
 
 LoginForm.defaultProps = {
+  authenticationProviders: [],
   logo: defaultLogo,
   setLoggedIn: loggedIn => console.log(loggedIn)
 };
