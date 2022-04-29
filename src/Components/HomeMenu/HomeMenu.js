b53a6cb85109ba6f338c1f0d7f4a631fad61526a
Matheus Fernandes <matheus.fernandes@mov.ai>
2022-04-29 15:50:26 +0000

    Implement home menu and home menu skeleton

---
+++ src/Components/HomeMenu/HomeMenu.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Button, Divider, IconButton } from "@material-ui/core";
+import { User, Utils } from "@mov-ai/mov-fe-lib-core";
+import AppsIcon from "@material-ui/icons/Apps";
+import { HomeMenuPopperStyles } from "./styles";
+import HomeMenuSkeleton from "./HomeMenuSkeleton";
+import HTMLPopper from "../Popper/HTMLPopper";
+
+const HomeMenuPopper = () => {
+  const classes = HomeMenuPopperStyles();
+  const [currentApps, setCurrentApps] = React.useState(false);
+
+  const currentUser = new User();
+
+  //========================================================================================
+  /*                                                                                      *
+   *                                    Private Methods                                   *
+   *                                                                                      */
+  //========================================================================================
+  const getIcon = () => {
+    return (
+      <IconButton color="primary" component="span">
+        <AppsIcon />
+      </IconButton>
+    );
+  };
+
+  /**
+   * Handle app click
+   * @param {Event} event : click event
+   * @param {object} element : App object
+   */
+  const onAppClick = (event, app) => {
+    if (app.Label !== undefined) {
+      Utils.loadResources(event, app);
+    }
+  };
+
+  //========================================================================================
+  /*                                                                                      *
+   *                                      SUBSCRIBERS                                     *
+   *                                                                                      */
+  //========================================================================================
+  /**
+   * subscribe to Applications updates
+   */
+  currentUser.getAllApps().then(res => {
+    res.success && setCurrentApps(res.result);
+  });
+
+  //========================================================================================
+  /*                                                                                      *
+   *                                        RENDERS                                       *
+   *                                                                                      */
+  //========================================================================================
+  const renderApplications = apps => {
+    return apps.map(app => (
+      <Button
+        key={app.URL}
+        size="large"
+        color="primary"
+        className={classes.menuButton}
+        onClick={event => onAppClick(event, app)}
+      >
+        <div className={classes.appTextArea}>
+          <div className={`${classes.appIcon} ${app.Icon}`} />
+          <div className={classes.appMiniature}>{app.Label}</div>
+        </div>
+      </Button>
+    ));
+  };
+
+  const renderHTML = () => {
+    if (currentApps) {
+      const arrayOfApplications = currentApps.filter(
+        app => app.Type === "application" && app.Label !== "launcher"
+      );
+      const arrayOfExternalApps = currentApps.filter(
+        app => app.Type === "external"
+      );
+      const arrayOfLayouts = currentApps.filter(app => app.Type === "layout");
+      return (
+        <>
+          <div className={classes.menuWrapper}>
+            {renderApplications(arrayOfApplications)}
+            {arrayOfLayouts.length > 1 && (
+              <>
+                <Divider orientation="horizontal" flexItem />
+                {renderApplications(arrayOfLayouts)}
+              </>
+            )}
+            {arrayOfExternalApps.length > 1 && (
+              <>
+                <Divider orientation="horizontal" flexItem />
+                {renderApplications(arrayOfExternalApps)}
+              </>
+            )}
+          </div>
+        </>
+      );
+    } else {
+      return <HomeMenuSkeleton />;
+    }
+  };
+
+  //========================================================================================
+  /*                                                                                      *
+   *                                        Render                                        *
+   *                                                                                      */
+  //========================================================================================
+  return (
+    <HTMLPopper
+      clickableElement={getIcon()}
+      hideOnClickAway={true}
+      popperPlacement="center"
+    >
+      {renderHTML()}
+    </HTMLPopper>
+  );
+};
+
+export default HomeMenuPopper;
