0b542351049e2def6e098022ef3d5354168d2a4b
Jose Sampaio <jose.sampaio@mov.ai>
2022-05-20 15:30:40 +0000

    More changes

--- src/Components/LoginForm/LoginForm.test.js
+++ src/Components/LoginForm/LoginForm.test.js
@@ -4,6 +4,10 @@ import "@testing-library/jest-dom";
 import LoginForm from "./LoginForm";
 import { Authentication } from "@mov-ai/mov-fe-lib-core";
 
+const SOME_DOMAIN_NAME = "OTHER_DOMAIN";
+const SINGLE_DOMAINS = [Authentication.DEFAULT_PROVIDER];
+const MULTIPLE_DOMAINS = [Authentication.DEFAULT_PROVIDER, SOME_DOMAIN_NAME];
+
 describe("Render", () => {
   it("renders the component (smoke test)", () => {
     const { container } = render(<LoginForm></LoginForm>);
@@ -28,7 +32,7 @@ describe("Initial value", () => {
 describe("Domain Selector", () => {
   it("Should not be visible if only default domain exists", () => {
     const { queryByText } = render(
-      <LoginForm domains={[Authentication.DEFAULT_PROVIDER]}></LoginForm>
+      <LoginForm domains={SINGLE_DOMAINS}></LoginForm>
     );
     const domainSelector = queryByText("Domain");
     expect(domainSelector).toBeNull();
@@ -36,26 +40,26 @@ describe("Domain Selector", () => {
 
   it("Should be visible if default and other domain exists", () => {
     const { queryByText } = render(
-      <LoginForm
-        domains={[Authentication.DEFAULT_PROVIDER, "otherDomain"]}
-      ></LoginForm>
+      <LoginForm domains={MULTIPLE_DOMAINS}></LoginForm>
     );
     const domainSelector = queryByText("Domain");
     expect(domainSelector).toBeInTheDocument();
   });
 
   it("Shoud display the domains", async () => {
-    const OTHER_DOMAIN = "OTHER_DOMAIN";
-    const domains = [Authentication.DEFAULT_PROVIDER, OTHER_DOMAIN];
-    const { getByRole } = render(<LoginForm domains={domains}></LoginForm>);
+    const { getByRole } = render(
+      <LoginForm domains={MULTIPLE_DOMAINS}></LoginForm>
+    );
 
-    const domainSelector = getByRole("button", { name: /internal/ });
+    const domainSelector = getByRole("button", {
+      name: Authentication.DEFAULT_PROVIDER
+    });
     fireEvent.mouseDown(domainSelector);
 
     const listbox = getByRole("listbox");
     const options = within(listbox).getAllByRole("option");
 
-    expect(options).toHaveLength(domains.length);
+    expect(options).toHaveLength(MULTIPLE_DOMAINS.length);
   });
 });
 
@@ -64,7 +68,7 @@ describe("Selected Domain", () => {
     const handleSubmit = jest.fn();
     const { queryByText, getByTestId } = render(
       <LoginForm
-        domains={[Authentication.DEFAULT_PROVIDER]}
+        domains={SINGLE_DOMAINS}
         onLoginSubmit={handleSubmit}
       ></LoginForm>
     );
@@ -106,33 +110,46 @@ describe("Selected Domain", () => {
 
 describe("Handle changes", () => {
   it("Selecting a value makes it the current domain value", () => {
-    const OTHER_DOMAIN = "OTHER_DOMAIN";
-
     const { getByTestId } = render(
-      <LoginForm
-        domains={[OTHER_DOMAIN, Authentication.DEFAULT_PROVIDER]}
-      ></LoginForm>
+      <LoginForm domains={MULTIPLE_DOMAINS}></LoginForm>
     );
     const domainSelector = getByTestId("input_domain");
     expect(domainSelector).toBeInTheDocument();
 
     fireEvent.select(domainSelector, {
-      target: { value: OTHER_DOMAIN }
+      target: { value: SOME_DOMAIN_NAME }
     });
 
-    expect(domainSelector.value).toBe(OTHER_DOMAIN);
+    expect(domainSelector.value).toBe(SOME_DOMAIN_NAME);
   });
 });
 
 describe("Submit", () => {
+  it("Should not be called if username is empty", () => {
+    const handleSubmit = jest.fn();
+    const { getByTestId } = render(
+      <LoginForm onLoginSubmit={handleSubmit}></LoginForm>
+    );
+
+    const usernameInput = getByTestId("input_username");
+    fireEvent.change(usernameInput, {
+      target: { value: "" }
+    });
+
+    const submitBtn = getByTestId("input_login");
+    fireEvent.click(submitBtn);
+
+    expect(handleSubmit).toBeCalledTimes(0);
+  });
+
   it("Should not be called if password is empty", () => {
     const handleSubmit = jest.fn();
     const { getByTestId } = render(
       <LoginForm onLoginSubmit={handleSubmit}></LoginForm>
     );
 
-    const password = getByTestId("input_password");
-    fireEvent.change(password, {
+    const passwordInput = getByTestId("input_password");
+    fireEvent.change(passwordInput, {
       target: { value: "" }
     });
 
