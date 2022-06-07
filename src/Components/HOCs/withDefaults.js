e41811344212060d6e0d9b871aa7027cbe83ba9b
Jose Sampaio <jose.sampaio@mov.ai>
2022-06-07 14:22:56 +0000

    Handle offline validation

--- src/Components/HOCs/withDefaults.js
+++ src/Components/HOCs/withDefaults.js
@@ -4,20 +4,15 @@ import withOfflineValidation from "./withOfflineValidation";
 import withTheme from "./withTheme";
 
 export default function withDefaults(appOptions, themeOptions) {
-  const { name: appName, component: appComponent } = appOptions;
+  const {
+    name: appName,
+    component: appComponent,
+    offlineValidation = true
+  } = appOptions;
   const { provider: themeProvider, theme: applicationTheme } = themeOptions;
-  console.log("appName: ", appName, " appComponent: ", appComponent);
-  console.log(
-    "themeProvider: ",
-    themeProvider,
-    "ApplicationTheme: ",
-    applicationTheme
-  );
+  if (offlineValidation) appComponent = withOfflineValidation(appComponent);
   return withTheme(
-    withAuthentication(
-      withNotification(withOfflineValidation(appComponent)),
-      appName
-    ),
+    withAuthentication(withNotification(appComponent), appName),
     themeProvider,
     applicationTheme
   );
