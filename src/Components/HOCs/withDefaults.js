0fe607505a96b00ada1158db65211afd44504f8c
Jose Sampaio <jose.sampaio@mov.ai>
2022-06-07 16:47:55 +0000

    Use only one param for now

--- src/Components/HOCs/withDefaults.js
+++ src/Components/HOCs/withDefaults.js
@@ -3,18 +3,19 @@ import withNotification from "./withNotification";
 import withOfflineValidation from "./withOfflineValidation";
 import withTheme from "./withTheme";
 
-export default function withDefaults(appOptions, themeOptions = {}) {
+export default function withDefaults(appOptions) {
   const {
     name: appName,
     component: appComponent,
-    offlineValidation = true
+    offlineValidation = true,
+    theme = {}
   } = appOptions;
-  let componentWithDefaults = { ...appComponent };
+  let componentWithDefaults = appComponent;
   if (offlineValidation)
     componentWithDefaults = withOfflineValidation(componentWithDefaults);
   return withTheme(
     withAuthentication(withNotification(componentWithDefaults), appName),
-    themeOptions.provider,
-    themeOptions.theme
+    theme.provider,
+    theme.props
   );
 }
