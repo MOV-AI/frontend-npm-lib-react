89f264ff99c4a81efdbdef21635c85ce90865aa3
Jose Sampaio <jose.sampaio@mov.ai>
2022-06-07 14:39:51 +0000

    Handle undefined theme options

--- src/Components/HOCs/withDefaults.js
+++ src/Components/HOCs/withDefaults.js
@@ -3,15 +3,18 @@ import withNotification from "./withNotification";
 import withOfflineValidation from "./withOfflineValidation";
 import withTheme from "./withTheme";
 
-export default function withDefaults(appOptions, themeOptions) {
-  const { name: appName, component: appComponent } = appOptions;
-  const { provider: themeProvider, theme: applicationTheme } = themeOptions;
+export default function withDefaults(appOptions, themeOptions = {}) {
+  const {
+    name: appName,
+    component: appComponent,
+    offlineValidation = true
+  } = appOptions;
+  let componentWithDefaults = { ...appComponent };
+  if (offlineValidation)
+    componentWithDefaults = withOfflineValidation(appComponent);
   return withTheme(
-    withAuthentication(
-      withNotification(withOfflineValidation(appComponent)),
-      appName
-    ),
-    themeProvider,
-    applicationTheme
+    withAuthentication(withNotification(componentWithDefaults), appName),
+    themeOptions.provider,
+    themeOptions.theme
   );
 }
