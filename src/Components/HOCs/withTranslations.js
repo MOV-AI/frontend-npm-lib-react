5bea006ff2025c20024b72503bedb3825513b9a4
Jose Sampaio <jose.sampaio@mov.ai>
2022-06-22 17:13:23 +0000

    Add HOC to initialize Translations the same way for all Apps

---
+++ src/Components/HOCs/withTranslations.js
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import React from "react";
+import { initReactI18next } from "react-i18next";
+import { getTranslationsConfig } from "../../i18n";
+
+export default function withTranslations(
+  Component,
+  { files, provider: I18nextProvider }
+) {
+  return function (props) {
+    const config = getTranslationsConfig(files);
+    i18n.use(initReactI18next).init(config);
+
+    return (
+      <I18nextProvider i18n={i18n}>
+        <Component {...props} />
+      </I18nextProvider>
+    );
+  };
+}
--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,37 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import { getTranslationsConfig } from "./";
 
 import translationEN from "./locales/en/translation.json";
 import translationPT from "./locales/pt/translation.json";
 
-// the translations
-const resources =
-  window.SERVER_DATA === undefined || window.SERVER_DATA.Languages === undefined
-    ? {
-        en: { translation: translationEN },
-        pt: { translation: translationPT }
-      }
-    : window.SERVER_DATA.Languages;
-
-// default language
-const language =
-  window.SERVER_DATA === undefined || window.SERVER_DATA.Language === undefined
-    ? "en"
-    : window.SERVER_DATA.Language;
+const translationsConfig = getTranslationsConfig({
+  en: translationEN,
+  pt: translationPT
+});
 
 i18n
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    resources,
-    lng: language,
-    fallbackLng: "en",
-    debug: false,
-
-    interpolation: {
-      escapeValue: false // not needed for react as it escapes by default
-    }
-  });
+  .init(translationsConfig);
 
 export default i18n;
