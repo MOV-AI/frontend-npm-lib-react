8959171d15cbd911d678269c5c9370459907ce83
José Sampaio <97457598+jebesampaio@users.noreply.github.com>
2022-07-07 14:05:51 +0100

    Update src/i18n/locales/index.js
    Co-authored-by: Manuel Nogueira <94365419+manuelnogueiramov@users.noreply.github.com>

--- src/i18n/locales/index.js
+++ src/i18n/locales/index.js
@@ -1,7 +1,23 @@
 import translationEN from "./en/translation.json";
 import translationPT from "./pt/translation.json";
 
-export const Translations = {
-  pt: translationPT,
-  en: translationEN
-};
+const AVAILABLE_TRANSLATIONS = {
+  ENGLISH: {
+     ABBR: "en",
+     LANGUAGE: "English",
+     DESCRIPTION: "UK English",
+     TRANSLATION: translationEN,
+     ACTIVE: true
+  },
+  PORTUGUESE: {
+     ABBR: "pt",
+     LANGUAGE: "Português",
+     DESCRIPTION: "Português de Portugal",
+     TRANSLATION: translationPT,
+     ACTIVE: true
+  }
+}
+export const Translations = Object.values(AVAILABLE_TRANSLATIONS).reduce((a, curr) => {
+    const newLang = curr.ACTIVE ? {[curr.ABBR]: curr.TRANSLATION} : {};
+    return {...a, ...newLang}
+}, {});
