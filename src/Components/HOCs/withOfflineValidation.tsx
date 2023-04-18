0dd67f960e36591ad3c9a83f8b2e29a520cacb0d
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2023-04-18 15:23:48 +0100

    fixup! - [FP-2231: With offline snackbar size is wrong](https://movai.atlassian.net/browse/FP-2231)

--- src/Components/HOCs/withOfflineValidation.tsx
+++ src/Components/HOCs/withOfflineValidation.tsx
@@ -5,8 +5,8 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/styles";
 import { Button } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
-  alert: { position: "fixed", bottom: 0, width: "calc(100% - 32px)", zIndex: 9999 }
+const useStyles = makeStyles(() => ({
+  alert: { position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 9999 }
 }));
 
 export default function withOfflineValidation(Component: React.ComponentType) {
