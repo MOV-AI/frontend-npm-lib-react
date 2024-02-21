7054f8dd3d11692af7757c5a44ed192fbb6f54d1
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2024-02-21 14:20:35 +0000

    Add optional service column

--- src/Components/Logs/utils/Constants.js
+++ src/Components/Logs/utils/Constants.js
@@ -19,6 +19,7 @@ export const COLUMNS_LABEL = {
   time: "Time",
   level: "Level",
   module: "Module",
+  service: "Service",
   robot: "Robot",
   message: "Message"
 };
@@ -41,6 +42,7 @@ export const DEFAULT_COLUMNS = {
   time: true,
   level: false,
   module: false,
+  service: false,
   robot: true,
   message: true,
 };
