a47d29d4ab898643a55d5fcf2e9d8be4d685575b
Paulo Andre Azevedo Quirino <paulo.quirino@mov.ai>
2024-03-10 22:50:37 +0000

    Restore mui

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,3 @@
-// babel.config.js
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          chrome: "78",
-          node: "12"
-        }
-      }
-    ],
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ],
-  plugins: [
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-transform-runtime", { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ["@babel/plugin-transform-private-property-in-object", { loose: true }]
-  ]
+  extends: "./node_modules/@tty-pt/scripts/babel.config.js"
 };
