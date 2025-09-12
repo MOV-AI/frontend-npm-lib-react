a7129b34fb1ea5432c1f620635fa762d53c9ee69
pedro-cristovao <pedro.cristovao@mov.ai>
2025-09-12 10:37:51 +0000

    refactor: update withAuthentication test to include async wait for Username input

--- src/Components/HOCs/withAuthentication.test.tsx
+++ src/Components/HOCs/withAuthentication.test.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import withAuthentication from "./withAuthentication";
 import withTheme from "./withTheme";
 
 const MockComponent = () => <div>hello</div>;
 
 describe("Render", () => {
-  it("renders the component (smoke test)", () => {
+  it("renders the component (smoke test)", async () => {
     const HOC = withAuthentication(MockComponent, "testApp");
     const Themed = withTheme(HOC);
 
     const { getByText } = render(<Themed />);
-    expect(getByText("Username")).toBeInTheDocument();
+    expect(getByText("Preparing the bots")).toBeInTheDocument();
+
+    // await for Username input to be rendered
+    await waitFor(
+      () => {
+        expect(getByText("Username")).toBeInTheDocument();
+      },
+      { timeout: 5000 },
+    );
   });
 });
